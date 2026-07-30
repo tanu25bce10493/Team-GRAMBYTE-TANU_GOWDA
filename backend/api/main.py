d424d8634bfeea26d50b37a4dac38ba672673438
udit25bai10827-ai <udit.25bai10827@vitbhopal.ac.in>
2026-07-30 19:25:00 +0530

    booking endpoint main file

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,1 +1,15 @@
-from backend.api.bridge_client import process_booking
+@app.post(
+    "/api/book",
+    response_model=BookingResponse,
+    status_code=status.HTTP_202_ACCEPTED
+)
+def create_booking(booking: BookingRequest):
+
+    bridge_response = process_booking(booking)
+
+    return BookingResponse(
+        success=bridge_response["success"],
+        status=bridge_response["status"],
+        message=bridge_response["message"],
+        booking=booking,
+    )
