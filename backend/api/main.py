0d7d84f550af60f21353ce934d5acd828a3d498d
udit25bai10827-ai <udit.25bai10827@vitbhopal.ac.in>
2026-07-30 21:51:19 +0530

    creating the fastapi app

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,3 +3,13 @@ from backend.api.exceptions import (
     booking_exception_handler,
     generic_exception_handler,
 )
+
+app.add_exception_handler(
+    BookingException,
+    booking_exception_handler,
+)
+
+app.add_exception_handler(
+    Exception,
+    generic_exception_handler,
+)
