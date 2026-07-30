05f06f174fea520c237ac4a6f682d413d1833e52
udit25bai10827-ai <udit.25bai10827@vitbhopal.ac.in>
2026-07-30 21:37:50 +0530

    add the new import

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,15 +1,1 @@
-@app.post(
-    "/api/book",
-    response_model=BookingResponse,
-    status_code=status.HTTP_202_ACCEPTED
-)
-def create_booking(booking: BookingRequest):
-
-    bridge_response = process_booking(booking)
-
-    return BookingResponse(
-        success=bridge_response["success"],
-        status=bridge_response["status"],
-        message=bridge_response["message"],
-        booking=booking,
-    )
+from backend.api.services import book_resource
