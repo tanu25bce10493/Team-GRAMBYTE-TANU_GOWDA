ff9c81da359a3d5365c1f50ee40803b5ef9358ed
Tanu-codeveda <tanugowda2468@gmail.com>
2026-07-30 17:58:35 +0530

    This represents a single bookable resource

---
+++ backend/cpp/include/Resource.hpp
@@ -0,0 +1,15 @@
+// backend/cpp/include/Resource.hpp
+
+#ifndef RESOURCE_HPP
+#define RESOURCE_HPP
+
+#include <string>
+
+struct Resource
+{
+    int id;
+    std::string name;
+    int capacity;
+};
+
+#endif
