7bc806b178562c2ad62e597ef8067d445d5844ab
udit25bai10827-ai <udit.25bai10827@vitbhopal.ac.in>
2026-07-30 19:24:04 +0530

    import process booking

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,17 +1,1 @@
-from fastapi import status
-
-# ...
-
-@app.post(
-    "/api/book",
-    response_model=BookingResponse,
-    status_code=status.HTTP_202_ACCEPTED
-)
-def create_booking(booking: BookingRequest):
-
-    return BookingResponse(
-        success=True,
-        status=status.HTTP_202_ACCEPTED,
-        message="Booking request accepted",
-        booking=booking,
-    )
+from backend.api.bridge_client import process_booking
