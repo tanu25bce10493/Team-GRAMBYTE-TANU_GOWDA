e78520d9066cddefc021780d70d4c3862264c45a
Tanu-codeveda <tanugowda2468@gmail.com>
2026-07-30 22:51:17 +0530

    few changes (changed userid to boookedby)

--- backend/cpp/src/main.cpp
+++ backend/cpp/src/main.cpp
@@ -7,20 +7,22 @@ int main()
     GraphEngine engine;
 
     // Create a sample resource
-    Resource lab;
-    lab.id = 1;
-    lab.name = "Computer Lab";
-    lab.capacity = 40;
+    Resource lab{
+    1,
+    "Computer Lab",
+    40
+    };
 
     engine.addResource(lab);
 
     // First booking
-    Booking booking1;
-    booking1.bookingId = 1;
-    booking1.resourceId = 1;
-    booking1.userId = "Alice";
-    booking1.startTime = "10:00";
-    booking1.endTime = "11:00";
+    Booking booking1{
+    1,
+    1,
+    "Alice",
+    "10:00",
+    "11:00"
+    };
 
     if (engine.createBooking(booking1))
     {
@@ -35,18 +37,16 @@ int main()
     Booking booking2;
     booking2.bookingId = 2;
     booking2.resourceId = 1;
-    booking2.userId = "Bob";
+    booking2.bookedBy = "Bob";
     booking2.startTime = "10:30";
     booking2.endTime = "11:30";
 
-    if (engine.createBooking(booking2))
-    {
-        std::cout << "Booking 2 Successful\n";
-    }
-    else
-    {
-        std::cout << "Booking 2 Failed (Conflict Detected)\n";
-    }
+   std::cout
+    << "Booking 2: "
+    << (engine.createBooking(booking2)
+        ? "Successful"
+        : "Failed (Conflict Detected)")
+    << '\n';
 
     return 0;
 }
