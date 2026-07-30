659283a2efcc9bab61fd2c796e7bb1a7d1d487b7
udit25bai10827-ai <udit.25bai10827@vitbhopal.ac.in>
2026-07-30 21:39:18 +0530

    replace the booking end point

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,1 +1,15 @@
-from backend.api.services import book_resource
+@app.post(
+    "/api/book",
+    response_model=BookingResponse,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def create_booking(booking: BookingRequest):
+
+    result = book_resource(booking)
+
+    return BookingResponse(
+        success=result["success"],
+        status=result["status"],
+        message=result["message"],
+        booking=booking,
+    )
