bbe3da632718c463ccc5ce2dee43287cd9b5013c
bhushankkhune07 <bhushan.25bcy10090@vitbhopal.ac.in>
2026-07-30 17:46:58 +0530

    Creates Constatnts

--- backend/bridge/logger.py
+++ backend/bridge/logger.py
@@ -1,3 +1,15 @@
 import json
 import os
 from datetime import datetime
+
+
+DATABASE_FOLDER = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "database"
+)
+
+JOURNAL_FILE = os.path.join(
+    DATABASE_FOLDER,
+    "journal.json"
+)
