37e49bdfbf37f92a9e272b7b414a6700e741a7d8
Tanu-codeveda <tanugowda2468@gmail.com>
2026-07-30 18:33:55 +0530

    Boilerplate for GraphEngine

---
+++ backend/cpp/src/GraphEngine.cpp
@@ -0,0 +1,11 @@
+#include "../include/GraphEngine.hpp"
+
+void GraphEngine::addResource(const Resource& resource)
+{
+    resources.push_back(resource);
+}
+
+void GraphEngine::addBooking(const Booking& booking)
+{
+    bookings.push_back(booking);
+}
